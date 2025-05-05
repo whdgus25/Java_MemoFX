d64ab0ce1d69087f86b4530bc0c744864443ef72
whdgus25 <jonghyun7943@gmail.com>
2025-05-05 11:34:42 +0900

    V0.2

--- src/main/java/org/memo/MemoApp.java
+++ src/main/java/org/memo/MemoApp.java
@@ -2,10 +2,7 @@ package org.memo;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,18 +10,27 @@ public class MemoApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        TextField titleField = new TextField();
+        titleField.setPromptText("제목을 입력하세요.");
 
         TextArea memoArea = new TextArea();
         Button saveButton = new Button("저장");
 
         saveButton.setOnAction(e -> {
-           String content = memoArea.getText();
-           MemoManager.saveMemo(content);
-           memoArea.clear();
-           showAlert("저장 완료", "메모가 저장되었습니다.");
+            String title = titleField.getText().trim();
+            String content = memoArea.getText().trim();
+
+            if (title.isEmpty() || content.isEmpty()) {
+                showAlert("입력 오류", "제목과 내용을 모두 입력해주세요.");
+            } else {
+                MemoManager.saveMemo(title, content);
+                titleField.clear();
+                memoArea.clear();
+                showAlert("저장 완료", "메모가 저장되었습니다.");
+            }
         });
 
-        VBox layout = new VBox(10, new Label("메모 입력: "), memoArea, saveButton);
+        VBox layout = new VBox(10, new Label("제목: "), titleField, new Label("메모 내용: "), memoArea, saveButton);
         Scene scene = new Scene(layout, 400, 300);
 
         primaryStage.setTitle("메모장");
--- src/main/java/org/memo/MemoManager.java
+++ src/main/java/org/memo/MemoManager.java
@@ -11,14 +11,15 @@ public class MemoManager {
 
     private static final File memoFolder = new File("memos");
 
-    public static void saveMemo(String content) {
+    public static void saveMemo(String title, String content) {
         if (!memoFolder.exists()) memoFolder.mkdirs();
 
         String timestamp = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
         File memoFile = new File(memoFolder, timestamp + ".txt");
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(memoFile))) {
-            writer.write(content);
+            writer.write("제목: " + title + "\n");
+            writer.write("내용: " + content);
         } catch (IOException e) {
             e.printStackTrace();
         }
